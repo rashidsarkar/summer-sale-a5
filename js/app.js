4d73f0092e0c7744548d4baeac6318069cfb408f
rashidsarkar <rashidrock558@gmail.com>
2023-08-20 16:32:14 +0600

    price update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const totalPrice = document.getElementById("total-price");
 const discount = document.getElementById("discount");
 const total = document.getElementById("total");
+const purchaseBtn = document.getElementById("Purchase-btn");
+const coponBtn = document.getElementById("copon-btn");
+const promo = document.getElementById("promo");
+const myDiscount = 20;
+let myDiscountPrice = 0;
 function stringtoNumber(num) {
   const elementNumber = parseFloat(num);
   return elementNumber.toFixed(2);
@@ -21,4 +26,22 @@ function handleClick(target) {
   const itemPriceNumber = parseFloat(itemPriceWithFloat);
   myTotalPrice += itemPriceNumber;
   totalPrice.textContent = myTotalPrice.toFixed(2);
+  total.textContent = myTotalPrice.toFixed(2);
+  if (myTotalPrice > 0) {
+    purchaseBtn.classList.remove("btn-disabled");
+  }
+  if (myTotalPrice >= 200) {
+    coponBtn.classList.remove("btn-disabled");
+  }
 }
+coponBtn.addEventListener("click", function () {
+  const totalPriceForCoponString = totalPrice.textContent;
+  const totalPriceForCoponNumber = parseFloat(totalPriceForCoponString);
+  const promoCode = promo.value;
+  const myCopon = "SELL200";
+  if (promoCode === myCopon) {
+    myDiscountPrice = (myDiscount / 100) * totalPriceForCoponNumber;
+    discount.textContent = myDiscountPrice.toFixed(2);
+    total.textContent = (totalPriceForCoponString - myDiscountPrice).toFixed(2);
+  }
+});
